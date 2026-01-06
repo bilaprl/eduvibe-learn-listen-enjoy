c4166110952e80f4fc97bd9621e189632573cbb9
bilaprl <apriliantinabila04@gmail.com>
2026-01-06 09:22:51 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "EduVibe - Learn • Listen • Enjoy",
+  title: "EduVibe - Learn • Listen • Enjoy - ena",
   description: "Media Pembelajaran Musik Interaktif",
 };
 
